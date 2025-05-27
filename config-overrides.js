5ba8f0d0645e5331d93635affaad7f06024a9bd7
stack <stackbolier@gmail.com>
2025-05-27 13:38:29 +0000

    updated config

--- config-overrides.js
+++
@@ -1,8 +0,0 @@
-const path = require("path");
-
-module.exports = {
-  webpack: (config) => {
-    config.output.path = path.resolve(__dirname, "build"); // Change output directory to "dist"
-    return config;
-  },
-};
